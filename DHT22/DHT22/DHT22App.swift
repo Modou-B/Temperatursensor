d51ac343f64f378886edbe5c45790be25298581e
Modou-B <modou.be@gmail.com>
2023-06-25 18:28:18 +0200

    init

---
+++ DHT22/DHT22/DHT22App.swift
@@ -0,0 +1,17 @@
+//
+//  DHT22App.swift
+//  DHT22
+//
+//  Created by Modou on 25.06.23.
+//
+
+import SwiftUI
+
+@main
+struct DHT22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
